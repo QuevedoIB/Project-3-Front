6c7427143d7be2bed2b11b85f0f7bf34a07abaff
Ivan <4esoquevedoi@gmail.com>
2019-03-27 17:20:06 +0100

    profile setup

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { withAuth } from '../providers/AuthProvider';
+import { Link } from 'react-router-dom';
 
-export default class Profile extends Component {
+class Profile extends Component {
   render() {
+    console.log(this.props.user)
+    const { username } = this.props;
     return (
-      <div>
-
-      </div>
+      <section>
+        <div>
+          <h1>{username}</h1>
+          <img src='' alt={username} />
+        </div>
+        <p>quote</p>
+        <div>
+          <Link to='search-people'>Search People</Link>
+          <Link to='search-people'>Contacts</Link>
+        </div>
+      </section>
     )
   }
 }
+
+export default withAuth(Profile);
