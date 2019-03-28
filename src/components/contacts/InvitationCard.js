0df66a4f15bc19a99f7f019fa0739fee8ff65fa2
Ester Fernandez <esterfernand95@gmail.com>
2019-03-28 16:47:44 +0100

    added methods to contact list

--- src/components/contacts/InvitationCard.js
+++ src/components/contacts/InvitationCard.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-export default class ContactCard extends Component {
+export default class InvitationCard extends Component {
   render() {
     const { imageUrl, username, quote, id } = this.props.contact;
     return (
       <div>
         <img src={imageUrl} alt={username} />
         <div>
-        <Link to={`/contact/${id}`}><h3>{username}</h3></Link>
+          <Link to={`/contact/${id}`}><h3>{username}</h3></Link>
           <p>{quote}</p>
         </div>
         <div>
-          <Link to={`/chat/${id}`}>Chat</Link>
+          <button>Accept</button>
+          <button>Decline</button>
         </div>
         <button onClick={this.props.onDelete(id)}>Delete Match</button>
       </div>
