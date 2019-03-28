ae15e24633cd69ff1368330da1e22a75859fe00b
Ivan <4esoquevedoi@gmail.com>
2019-03-28 18:04:14 +0100

    fixed search people inputs

--- src/components/signup/InfoFields.js
+++ src/components/signup/InfoFields.js
@@ -6,11 +6,11 @@ export default class InfoFields extends Component {
     return (
       <div className="column-content form-fields">
         <label htmlFor='username'>Username:</label>
-        <input type="text" name="username" value={this.props.username} onChange={this.props.handleChange} />
+        <input type="text" name="username" value={this.props.username} onChange={this.props.handleChange} required />
         <label htmlFor='password'>Password:</label>
-        <input type="password" name="password" value={this.props.password} onChange={this.props.handleChange} />
+        <input type="password" name="password" value={this.props.password} onChange={this.props.handleChange} required />
         <label htmlFor='email'>Email:</label>
-        <input type="email" name="email" value={this.props.email} onChange={this.props.handleChange} />
+        <input type="email" name="email" value={this.props.email} onChange={this.props.handleChange} required />
 
         {/* comprobar si se suben las imágenes */}
 
