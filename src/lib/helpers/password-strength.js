fe843fcba644f6d0a9639fc77df9b0419e6a6e9f
Ivan <4esoquevedoi@gmail.com>
2019-03-31 12:21:37 +0200

    scss setup

--- src/lib/helpers/password-strength.js
+++ src/lib/helpers/password-strength.js
@@ -13,42 +13,40 @@ export const passwordStrengthCheck = () => {
   const meter = document.getElementById('password-strength-meter');
   const strengthText = document.getElementById('password-strength-text');
 
-  password.addEventListener('input', function () {
-    let val = password.value;
+  let val = password.value;
 
-    if (!val) {
+  if (!val) {
+    meter.className = 'hide';
+  }
+
+  let result = zxcvbn(val);
+
+  // Update the password strength meter
+  switch (result.score) {
+    case 0:
+      meter.className = 'worst-meter';
+      break;
+    case 1:
+      meter.className = 'bad-meter';
+      break;
+    case 2:
+      meter.className = 'weak-meter';
+      break;
+    case 3:
+      meter.className = 'good-meter';
+      break;
+    case 4:
+      meter.className = 'strong-meter';
+      break;
+    default:
       meter.className = 'hide';
-    }
+  }
+  meter.value = result.score;
 
-    let result = zxcvbn(val);
-
-    // Update the password strength meter
-    switch (result.score) {
-      case 0:
-        meter.className = 'worst-meter';
-        break;
-      case 1:
-        meter.className = 'bad-meter';
-        break;
-      case 2:
-        meter.className = 'weak-meter';
-        break;
-      case 3:
-        meter.className = 'good-meter';
-        break;
-      case 4:
-        meter.className = 'strong-meter';
-        break;
-      default:
-        meter.className = 'hide';
-    }
-    meter.value = result.score;
-
-    // Update the text indicator
-    if (val !== '') {
-      strengthText.innerText = 'Password Strength: ' + strength[result.score];
-    } else {
-      strengthText.innerText = '';
-    }
-  });
+  // Update the text indicator
+  if (val !== '') {
+    strengthText.innerText = 'Password Strength: ' + strength[result.score];
+  } else {
+    strengthText.innerText = '';
+  }
 };
