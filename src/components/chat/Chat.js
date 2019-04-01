a451bdede94c1c972e4f608774974434c7a58b29
Ester Fernandez <esterfernand95@gmail.com>
2019-04-01 22:39:44 +0200

    chat css done

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Messages from './Messages';
+import '../components-scss/chat.scss';
 
 class Chat extends Component {
 
@@ -19,10 +20,10 @@ class Chat extends Component {
                 <div>
                     <Messages chat={this.props.chat} contact={this.props.contact} />
                 </div>
-                <div>
+                <div className="send-content">
                     <form onSubmit={(e) => { e.preventDefault(); this.props.handleSendMessage(this.state.message); this.setState({ message: '', }) }} className="message-form">
-                        <input autoComplete="off" className="input message-input" placeholder="Write a message" type="text" name="message" onChange={this.handleChange} value={this.state.message} />
                         <input className="button message-button" type="submit" value="Send" />
+                        <input autoComplete="off" className="input message-input" placeholder="Write a message" type="text" name="message" onChange={this.handleChange} value={this.state.message} />
                     </form>
                 </div>
             </>
